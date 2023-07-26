720c02b13d56852384c607b29a3f990d4c8fb4a0
0sik <99026631+0sik@users.noreply.github.com>
2023-07-26 09:35:49 +0900

    Update transfer.py

--- mp4togif/transfer.py
+++ mp4togif/transfer.py
@@ -1,22 +1,21 @@
 import imageio
 
-# Path to the input .mp4 file
+
 mp4_path = "000000.mp4"
 
-# Path to save the output .gif file
+
 gif_path = "000000.gif"
 
-# Read the .mp4 file
+
 video = imageio.get_reader(mp4_path)
 
-# Initialize the frames list
+
 frames = []
 
-# Iterate over each frame in the video
+
 for frame in video:
     frames.append(frame)
 
-# Save the frames as a .gif file
 imageio.mimsave(gif_path, frames, format="gif")
 
 print("Conversion complete!")
