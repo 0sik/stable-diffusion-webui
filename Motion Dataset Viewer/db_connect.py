199921c46186d8029446f9fed7aac11a3d68d49a
0sik <99026631+0sik@users.noreply.github.com>
2023-08-04 13:17:15 +0900

    Update db_connect.py

--- Motion Dataset Viewer/db_connect.py
+++ Motion Dataset Viewer/db_connect.py
@@ -3,14 +3,14 @@ import os
 
 class ConnectDB:
 
-    # 인스턴스 초기화
+
     def __init__(self, sql):
         self.sql = sql  
         self.data = None
 
         self.conn = pymysql.connect(host='localhost', user='root', password='1234', db='motionai', charset='utf8',
-                                    autocommit=True)  # DB와 연결합니다.
-        self.curs = self.conn.cursor(pymysql.cursors.DictCursor)  # sql문 수행을 위해 cursor 객체를 생성합니다.
+                                    autocommit=True) 
+        self.curs = self.conn.cursor(pymysql.cursors.DictCursor) 
 
     
     def execute(self, *args):
@@ -23,11 +23,11 @@ class ConnectDB:
         self.data = self.curs.fetchall()
         return self.data
 
-    # 인스턴스 삭제
+
     def __del__(self):
-        self.curs.close()  # cursor 객체를 닫습니다.
-        self.conn.close()  # DB연결을 해제합니다.
+        self.curs.close() 
+        self.conn.close()  
 
     def connect():
             return pymysql.connect(host='localhost', user='root', password='1234', db='motionai', charset='utf8',
-                                autocommit=True)  # Connect to the MySQL database.
+                                autocommit=True) 
