aa38208444f1b30aec93e3c224339218c7d624e7
0sik <99026631+0sik@users.noreply.github.com>
2023-08-10 09:12:42 +0900

    Update create_database.sql

--- Motion Dataset Viewer/create_database.sql
+++ Motion Dataset Viewer/create_database.sql
@@ -4,22 +4,22 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
+
 -- Schema motionai
--- -----------------------------------------------------
+
 -- 모션 디퓨전 데이터베이스 
 
--- -----------------------------------------------------
+
 -- Schema motionai
 --
 -- 모션 디퓨전 데이터베이스 
--- -----------------------------------------------------
+
 CREATE SCHEMA IF NOT EXISTS `motionai` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 USE `motionai` ;
 
--- -----------------------------------------------------
+
 -- Table `motionai`.`mo_dataset`
--- -----------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `motionai`.`mo_dataset` (
   `idx` CHAR(6) NOT NULL,
   `kind` INT NOT NULL COMMENT '1: humanml\n2: kit \n3: custom ',
